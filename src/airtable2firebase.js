78761ea6c12c107a782c8fa5edcc1314735b369e
Enrico Scherlies <e.scherlies@me.com>
2018-05-28 13:29:52 +0200

    added backup func

--- src/airtable2firebase.js
+++ src/airtable2firebase.js
@@ -1,17 +1,23 @@
 import _ from 'lodash'
 import processTable from './processTable'
+import backupFirebaseDatabase from './backupFirebaseDatabase';
 
 const logGreen = text => console.log('\x1b[32m' + text + '\x1b[0m');
 
 const airtable2firebase = (...args) => {
     console.log('\x1b[36m%s\x1b[0m', '\n*** Airtable to Firebase ***\n');  //yellow
 
-    const tableProcessingPromisses = _.map(args, tableName => processTable(tableName))
+    // back up the firebase db
+    backupFirebaseDatabase()
+        .then(() => {
 
-    Promise
-        .all(tableProcessingPromisses)
-        .then(() => { logGreen('All Done'); process.exit() })
-        .catch(error => { console.error(error); process.exit() })
+            const tableProcessingPromisses = _.map(args, tableName => processTable(tableName))
+
+            Promise
+                .all(tableProcessingPromisses)
+                .then(() => { logGreen('All Done'); process.exit() })
+                .catch(error => { console.error(error); process.exit() })
+        })
 }
 
 export default airtable2firebase
--- src/utils.js
+++ src/utils.js
@@ -7,3 +7,29 @@ export const isArtableAttachment = object => {
     return checkProperties(["id", "url", "filename", "size", "type"]) // file
         || checkProperties(["url", "width", "height"]) // thumbnail
 }
+
+
+export const getDateTime = () => {
+
+    const date = new Date()
+
+    let hour = date.getHours()
+    hour = (hour < 10 ? "0" : "") + hour
+
+    let min = date.getMinutes()
+    min = (min < 10 ? "0" : "") + min
+
+    let sec = date.getSeconds()
+    sec = (sec < 10 ? "0" : "") + sec
+
+    let year = date.getFullYear()
+
+    let month = date.getMonth() + 1
+    month = (month < 10 ? "0" : "") + month
+
+    let day = date.getDate()
+    day = (day < 10 ? "0" : "") + day
+
+    const dateTime =  year + "-" + month + "-" + day + "T" + hour + ":" + min + ":" + sec
+    return dateTime
+}
